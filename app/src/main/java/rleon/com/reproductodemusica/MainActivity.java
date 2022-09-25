4c40cdfc7f88ea2d2877e3879c6fdcf9b9940aaa
Ricardo Ignacio Gutiérrez León <59661365+RicardoLeon15@users.noreply.github.com>
2022-09-25 02:02:04 -0500

    Pantalla principal - biblioteca
    Primer commit, pantalla principal ya lista y funcional, navegación con jetpack navigation, barra de navegación funcional

--- app/src/main/java/rleon/com/reproductodemusica/MainActivity.java
+++ app/src/main/java/rleon/com/reproductodemusica/MainActivity.java
@@ -1,14 +1,35 @@
 package rleon.com.reproductodemusica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Biblioteca biblioteca = new Biblioteca();
+    Canciones canciones = new Canciones();
+    Listas listas = new Listas();
+    Buscar buscar = new Buscar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
+
 }
