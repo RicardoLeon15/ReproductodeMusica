11794506a8a013e6f9e10a975d59f4fbc58a5cbb
Ricardo Ignacio Gutiérrez León <59661365+RicardoLeon15@users.noreply.github.com>
2022-10-07 02:53:57 -0500

    list part 2

--- app/src/main/java/rleon/com/reproductodemusica/AdapterList.java
+++ app/src/main/java/rleon/com/reproductodemusica/AdapterList.java
@@ -1,10 +1,16 @@
 package rleon.com.reproductodemusica;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.MediaMetadataRetriever;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,21 +18,35 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.util.ArrayList;
 
 public class AdapterList extends RecyclerView.Adapter<AdapterList.MHolder> {
 
     private Context mcontext;
     private ArrayList<ArchivosM> mFiles;
+    private String nomList;
+    private SharedPreferences spPlaylists;
+    private ArrayList<ArchivosM> arraylist = new ArrayList<ArchivosM>();
 
-    AdapterList(Context mcontext, ArrayList<ArchivosM> mFiles){
+    AdapterList(Context mcontext, ArrayList<ArchivosM> mFiles,String nomList){
         this.mFiles = mFiles;
         this.mcontext = mcontext;
+        this.nomList = nomList;
     }
 
     @NonNull
     @Override
     public MHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        spPlaylists = mcontext.getSharedPreferences(nomList, Context.MODE_PRIVATE);
+        String lists = spPlaylists.getString("Canciones","");
+        if (!lists.equals("")){
+            TypeToken<ArrayList<ArchivosM>> token = new TypeToken<ArrayList<ArchivosM>>(){};
+            Gson gson = new Gson();
+            arraylist = gson.fromJson(lists,token.getType());
+        }
         View view = LayoutInflater.from(mcontext).inflate(R.layout.itemaddlist,parent,false);
         return new MHolder(view);
     }
@@ -42,10 +62,16 @@ public class AdapterList extends RecyclerView.Adapter<AdapterList.MHolder> {
         }else{
             Glide.with(mcontext).load(R.drawable.ic_round_music_note_24).into(holder.icancion);
         }
-        holder.btnAdd.setOnClickListener(new View.OnClickListener() {
+        holder.btnAdd.setOnCheckedChangeListener(null);
+        holder.btnAdd.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(final View view) {
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    arraylist.add(mFiles.get(position));
+                }
+                else {
+                    arraylist.remove(mFiles.get(position));
+                }
             }
         });
     }
@@ -57,13 +83,15 @@ public class AdapterList extends RecyclerView.Adapter<AdapterList.MHolder> {
 
     public class MHolder extends RecyclerView.ViewHolder{
         TextView ncancion,datosCancion;
-        ImageView icancion, btnAdd;
+        ImageView icancion;
+        CheckBox btnAdd;
         public MHolder(@NonNull View itemView) {
             super(itemView);
             ncancion = itemView.findViewById(R.id.tvSongAddList);
             datosCancion = itemView.findViewById(R.id.tvADAddList);
             icancion = itemView.findViewById(R.id.imageAddList);
             btnAdd = itemView.findViewById(R.id.btnOptAddList);
+            freeMemory();
         }
     }
 
@@ -87,5 +115,15 @@ public class AdapterList extends RecyclerView.Adapter<AdapterList.MHolder> {
         }else{
             return totalOut;
         }
+    }
+
+    public void freeMemory(){
+        System.runFinalization();
+        Runtime.getRuntime().gc();
+        System.gc();
+    }
+
+    public ArrayList<ArchivosM> getArraylist() {
+        return arraylist;
     }
 }
